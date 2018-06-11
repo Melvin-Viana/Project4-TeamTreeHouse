69bc5a2d6ae6bd8c5af8cc5dc8d049cecca69e8e
Melvin <mjviana057@gmail.com>
2018-06-10 20:16:32 -0700

    newgame

--- js/newgame.js
+++ js/newgame.js
@@ -1,6 +1,20 @@
 $(document).ready(()=>{
+        var html = `
+        <div class="screen screen-start" id="start">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          </br>
+          <input type='text' placeholder='Name Of Player'></input></br></br>
+          <a href="#" class="button">Start game</a>
+        </header>
+      </div>`;
+      
+        //On Page load, load up the start up screen.
+      
  $('body').on('click','a.button',()=>{
         //Display New Game.
+        $("body").html(html);
+
 });
 
 });
